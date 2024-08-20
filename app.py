059e229f10bc5c43d638f71268fa393a14d30945
SirGamah <46490646+SirGamah@users.noreply.github.com>
2024-08-20 14:00:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -116,13 +116,11 @@ if selected == "Home":
     st.image("assets/resistAI_banner.png", use_column_width=True)
     st.subheader("Welcome to ResistAI")
     st.write("Some dummy texts here")
-    
-    if zip_file.type == "application/zip":
-        with zipfile.ZipFile(zip_file, "r") as z:
-            # Assuming 'atlas_gram_pos_cleaned_pop.csv' is inside the ZIP file
-            with z.open("atlas_gram_pos_cleaned_pop.csv") as csv_file:
-                df = pd.read_csv(csv_file)
-                st.write(df)
+    with zipfile.ZipFile(zip_file, "r") as z:
+        # Assuming 'atlas_gram_pos_cleaned_pop.csv' is inside the ZIP file
+        with z.open("atlas_gram_neg_cleaned_pop.zip") as csv_file:
+            df = pd.read_csv(csv_file)
+            st.write(df)
 
 
 if selected == "Analysis":
